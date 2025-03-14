6b09368aa1b9987b02ee0d3ca938ac6df6aa0c17
Steven Rydin <stevenrydin@gmail.com>
2025-03-14 14:52:59 -0600

    Add React application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BidAggregator from './components/BidAggregator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BidAggregator />
+    </div>
+  );
+}
+
+export default App;
